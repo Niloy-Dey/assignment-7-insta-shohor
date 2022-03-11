90115da361e3195acd7a828361980af713d7b3f1
Niloy-Dey <niloydeyce@gmail.com>
2022-03-11 22:59:01 +0600

    finaly work is done

--- js/app.js
+++ js/app.js
@@ -153,7 +153,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
     const likedPosts = getLikedPosts();
-    // console.log(likedPosts);
+    document.getElementById( "liked" ).innerHTML = "";
     likedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "liked" ).appendChild(div);
